9368abc90720b277872c92dedab9d14373f238bb
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-13 23:12:56 -0700

    Update FeatureToggle.js

--- FeatureToggle.js
+++ FeatureToggle.js
@@ -1,1 +1,16 @@
+import React from 'react';
+
+function FeatureToggle({ isEnabled, featureName }) {
+    return (
+        <div>
+            {isEnabled ? (
+                <h2>{featureName} is enabled</h2>
+            ) : (
+                <h2>Feature {featureName} is disabled</h2>
+            )}
+        </div>
+    );
+}
+
+export default FeatureToggle;
 
