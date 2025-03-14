f4d9134016cb5659fec6a59dfc598ba95420a0b9
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-13 23:16:31 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './App.css';
+import FeatureToggle from './FeatureToggle';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FeatureToggle isEnabled={true} featureName="New Dashboard" />
+        <FeatureToggle isEnabled={false} featureName="Dark Mode" />
+        <FeatureToggle isEnabled={true} featureName="Advanced Settings" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
 
